f09baeea06b3c215f6ca8ef23a72e737d9c42937
helther666 <helther666@HELTHER>
2020-02-27 22:50:03 +0300

    added some fixes

--- Source/__main__.py
+++ Source/__main__.py
@@ -1,6 +1,14 @@
-import DirProccessor
+from Source import processor
 import sys, getopt
-
+##############################################
+#todo global
+"""
+finish args parse
+get script run properly
+#add repo stcruct( license and readme)
+make public
+add tests"""
+##########################
 
 def displayTable(data, maxColLength=10,maxTableSize=500):
     """
@@ -21,68 +29,76 @@ def displayTable(data, maxColLength=10,maxTableSize=500):
     #todo add continuation if string longer than max
 
 def parseArgs():
+    #todo some docs
+    #todo replace getopt with argparse
+    shortArgs = "hds:m:n:t:r:"
     longArgs = ["help","directory","sortBy","minSize","nameFilter","typeFilter","rootDir"]
+    helpInfo = """"""
     try:
-        options, args = getopt.getopt(sys.argv[1:],"hds:m:n:t:r:",[longArgs])
-    except getopt.GetoptError:
-        print("arg error placeholder for proper usage")
+        options, args = getopt.getopt(sys.argv[1:],shortArgs,[longArgs])
+    except getopt.GetoptError as err:
+        print(str(err))
+        print(helpInfo)
         sys.exit(1)
     isDir = False
     reqs = {}
     for opt,arg in options:
         if opt in ("-h","--help"):
             #TODO add help
-            print("There will be usage help")
+            print(helpInfo)
+            sys.exit()
         if opt in ("-d","--directory"):
             isDir = True
         if opt in ("-s","--sortBy"):
             reqs["sortByName"] = arg
             ##TODO add assert
         else:
-            reqs["sortByName"] = DirProccessor.DefaultReqs["sortByName"]
+            reqs["sortByName"] = processor.DefaultReqs["sortByName"]
         if opt in ("-m","--minSize"):
             reqs["minSize"] = arg
         else:
-            reqs["minSize"] = DirProccessor.DefaultReqs["minSize"]
+            reqs["minSize"] = processor.DefaultReqs["minSize"]
         if opt in ("-n","--nameFilter"):
             reqs["nameFilter"] = arg
         else:
-            reqs["nameFilter"] = DirProccessor.DefaultReqs["nameFilter"]
+            reqs["nameFilter"] = processor.DefaultReqs["nameFilter"]
         if opt in ("-t","--typeFilter"):
             reqs["typeFilter"] = arg
         else:
-            reqs["typeFilter"] = DirProccessor.DefaultReqs["typeFilter"]
+            reqs["typeFilter"] = processor.DefaultReqs["typeFilter"]
         if opt in ("r","--rootDir"):
             reqs["rootDir"] = arg
         else:
-            reqs["rootDir"] = DirProccessor.DefaultReqs["rootDir"]
+            reqs["rootDir"] = processor.DefaultReqs["rootDir"]
     return reqs,isDir
-def main():pass
 # todo check path validity
 
 def testMenu():
     testIsDir = True
     defP = False
-    testReqs = {"sortBy" : DirProccessor.SortByWhat.NAME,
+    testReqs = {"sortBy" : processor.SortByWhat.NAME,
 				"minSize" :  0,
 				"nameFilter" : "",
 				"typeFilter" : "",
                 "rootDir" : "."}
     if testIsDir:
-        processor = DirProccessor.DirProc(testReqs)
+        processorTest = processor.DirProc(testReqs)
         if defP:
-            defProcessor =  DirProccessor.DirProc(None)
+            defProcessor =  processorTest.DirProc(None)
     else:
-        processor = DirProccessor.FileProc(testReqs)
+        processorTest = processor.FileProc(testReqs)
         if defP:
-            defProcessor = DirProccessor.FileProc(None)
-    resultData = processor.process()
+            defProcessor = processorTest.FileProc(None)
+    resultData = processorTest.process()
     displayTable(resultData)
     if defP:
         resultData = defProcessor.process()
         displayTable(resultData)
 
+
+def main(Argv):
+    testMenu()
+
 #######main
-testMenu()
-#if __name__ == '__main__':
-#main(sys.argv)
+if __name__ == '__main__':
+    main(sys.argv)
---
+++ Source/processor.py
@@ -0,0 +1,115 @@
+from enum import Enum
+import pathlib
+import os.path
+
+class SortByWhat(Enum):
+	NAME = 0
+	TYPE = 1
+	SIZE = 2
+
+#table of arguments default values
+DefaultReqs = 	{"sortBy" : SortByWhat.SIZE,
+				"minSize" :  0,
+				"nameFilter" : "",
+				"typeFilter" : "",
+                "rootDir" : '.'}
+
+class ProcessorBase(object):
+    def __init__(self,reqs = None):
+        self.reqs = DefaultReqs if reqs is None else reqs
+
+    def process(self): pass
+
+    def applyFilter(self,Name = DefaultReqs["nameFilter"],
+                    Size = DefaultReqs["minSize"],
+                    Type = DefaultReqs["typeFilter"]):
+        """
+        checks if elem passes the filter in reqs
+        :param Name string
+        :param Type string
+        :param Size int
+        :return: bool
+        """
+        if Name != DefaultReqs["nameFilter"] and self.reqs["nameFilter"] not in Name:
+            return False
+        if Type != DefaultReqs["typeFilter"] and self.reqs["typeFilter"] not in Type:
+            return False
+        if Size < self.reqs["minSize"]:
+            return False
+        return True
+
+class DirProc(ProcessorBase):
+    def __init__(self, reqs):
+        super(DirProc,self).__init__(reqs)
+
+    def dirScan(self,Sum,Dir):
+        """
+        goes through all elements in a given Directory
+        and adds sums of all files in them
+        :param Sum: list of ints ref
+        :param Dir: Path object
+        :return: None
+        """
+        for file in Dir.iterdir():
+            if file.is_dir():
+                self.dirScan(Sum,Dir / file.name)
+            else:
+                Sum[0]+=file.stat().st_size
+                
+    def process(self):
+        """
+        recursivly looks through sub files in a list of root Dir
+        and returns a list of Dirs names and it's sizes,
+        apllying filter, then sorts the resulting list
+        :return data: list<string,int>
+        """
+        rootP = pathlib.Path(self.reqs["rootDir"])
+        rootDirNames = []
+        for Dir in rootP.iterdir():
+            if Dir.is_dir():
+                rootDirNames.append(rootP / Dir)
+        data = []
+        for rootDir in rootDirNames:
+            if not self.applyFilter(Name=rootDir.name):
+                continue
+            Sum = [0]
+            self.dirScan(Sum,rootDir)
+            if self.applyFilter(Size=Sum[0]):
+                data.append((rootDir,Sum[0]))
+         
+        data.sort(key= lambda x:x[self.reqs["sortBy"].value])
+        return data
+
+class FileProc(ProcessorBase):
+    def __init__(self,reqs):
+        super(FileProc,self).__init__(reqs)
+
+    def fileScan(self,data,Dir):
+        """
+        goes through all elements in a given Directory
+        and adds them to a return list after appliyng filter
+        :param data: list<string,string,int>
+        :param Dir: Path object
+        :return: None
+        """
+        for file in Dir.iterdir():
+            if file.is_dir():
+                self.fileScan(data,Dir / file.name)
+            else:
+                fileName = os.path.splitext(file.name)[0]
+                fileExt = os.path.splitext(file.name)[1]
+                if self.applyFilter(fileName,file.stat().st_size,fileExt):
+                    data.append((fileName,fileExt,file.stat().st_size))
+                    
+    def process(self):
+        """
+        recursivly looks through sub files in a root Dir
+        and returns a list of files, then sorts the resulting list
+        :return data: list<string,string,int>
+        """
+        data = []
+        rootP = pathlib.Path(self.reqs["rootDir"])
+        self.fileScan(data,rootP)
+
+        data.sort(key= lambda x:x[self.reqs["sortBy"].value])
+        return data
