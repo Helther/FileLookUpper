82ecc32ce14d5171cfe10a7ff504ace82b215d4e
helther666 <helther666@HELTHER>
2020-04-05 22:06:58 +0300

    added project structure

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+version = '0.1'
+setuptools.setup(
+    name="FileLookUpper",
+    version=version,
+    author="Gusev Anton",
+    author_email="kaeldevop@gmail.com",
+    description="script for listing files/directories name according to filters",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Helther666/FileLookUpper.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "LICENSE :: OSI APPROVED :: APACHE SOFTWARE LICENSE",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    #test_suite='nose.collector',
+    #tests_require=['nose', 'simplejson'],
+)
